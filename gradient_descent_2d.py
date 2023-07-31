147779ae0df108e9fd1f9ef9bce05ab4901e1fae
hsiang086 <115937789+hsiang086@users.noreply.github.com>
2023-07-31 23:30:04 +0800

    tangent line

--- gradient_descent_2d.py
+++ gradient_descent_2d.py
@@ -7,12 +7,17 @@ def y_function(x):
 def y_derivative(x):
     return 2 * x
 
+def tangent_line(x):
+    return y_derivative(current_pos[0]) * (x - current_pos[0]) + current_pos[1]
+
 def init():
     global current_pos
     global x
     global y
+    global tangent_x
     x = np.arange(-100, 100, 0.1)
     y = y_function(x)
+    tangent_x = np.arange(0, 100, 0.1)
     current_pos = (80, y_function(80))
 
 learning_rate = 0.05
@@ -24,8 +29,9 @@ while True:
 
     plt.plot(x, y)
     plt.scatter(current_pos[0], current_pos[1], color="red")
-    plt.pause(0.01)
+    plt.plot(tangent_x, tangent_line(tangent_x), color="green")
+    plt.pause(.1)
     plt.clf()
 
-    if abs(y_derivative(current_pos[0])) < 0.01:
+    if abs(y_derivative(current_pos[0])) < 1:
         init()
