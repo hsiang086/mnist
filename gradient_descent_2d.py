dd21a023a6d936141bfac24533128302afdd529a
hsiang086 <115937789+hsiang086@users.noreply.github.com>
2023-07-31 23:06:30 +0800

    loop

--- gradient_descent_2d.py
+++ gradient_descent_2d.py
@@ -7,13 +7,16 @@ def y_function(x):
 def y_derivative(x):
     return 2 * x
 
-x = np.arange(-100, 100, 0.1)
-y = y_function(x)
-
-current_pos = (80, y_function(80))
+def init():
+    global current_pos
+    global x
+    global y
+    x = np.arange(-100, 100, 0.1)
+    y = y_function(x)
+    current_pos = (80, y_function(80))
 
 learning_rate = 0.05
-
+init()
 while True:
     new_x = current_pos[0] - learning_rate * y_derivative(current_pos[0])
     new_y = y_function(new_x)
@@ -23,3 +26,6 @@ while True:
     plt.scatter(current_pos[0], current_pos[1], color="red")
     plt.pause(0.01)
     plt.clf()
+
+    if abs(y_derivative(current_pos[0])) < 0.01:
+        init()
